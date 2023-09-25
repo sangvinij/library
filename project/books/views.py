99ce3cf164ede2fb0b758ef932aea0d4f883e6c8
Artyom Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2023-09-25 12:53:25 +0300

    test black fail

--- project/books/views.py
+++ project/books/views.py
@@ -30,6 +30,7 @@ class ReaderCreateView(View):
 class ReaderDetailView(View):
     queryset = Book.objects.select_related("author").select_related("reader")
 
+
     def get(self, request, pk):
         try:
             reader = Reader.objects.get(pk=pk)

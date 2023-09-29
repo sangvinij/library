5f11dde0d0e2a5416fed9194e5bca46a1b3f81e4
Artyom Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2023-09-29 19:45:02 +0300

    fix static9

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("books.urls"))]
+urlpatterns += staticfiles_urlpatterns()
